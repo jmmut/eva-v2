d20a5995aec6ecea64ee6cd38bc5df53a8baae7a
jose miguel mut <jomutlo@gmail.com>
2015-11-12 12:26:41 +0000

    test: split job. Adding more ways to build the job

--- eva-pipeline/src/test/java/embl/ebi/variation/eva/pipeline/jobs/splitJobTest.java
+++ eva-pipeline/src/test/java/embl/ebi/variation/eva/pipeline/jobs/splitJobTest.java
@@ -15,21 +15,10 @@
  */
 package embl.ebi.variation.eva.pipeline.jobs;
 
-import embl.ebi.variation.eva.pipeline.steps.VariantsLoad;
-import embl.ebi.variation.eva.pipeline.steps.VariantsStatsCreate;
-import embl.ebi.variation.eva.pipeline.steps.VariantsStatsLoad;
 import org.apache.commons.collections.list.SynchronizedList;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.opencb.biodata.models.variant.VariantSource;
-import org.opencb.datastore.core.QueryOptions;
-import org.opencb.opencga.storage.core.StorageManagerException;
-import org.opencb.opencga.storage.core.StorageManagerFactory;
-import org.opencb.opencga.storage.core.variant.VariantStorageManager;
-import org.opencb.opencga.storage.core.variant.adaptors.VariantDBAdaptor;
-import org.opencb.opencga.storage.core.variant.adaptors.VariantDBIterator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.*;
@@ -37,12 +26,10 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.builder.JobFlowBuilder;
+import org.springframework.batch.core.job.builder.SimpleJobBuilder;
 import org.springframework.batch.core.job.flow.Flow;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.builder.TaskletStepBuilder;
@@ -54,20 +41,18 @@ import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.UnknownHostException;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.zip.GZIPInputStream;
 
-import static embl.ebi.variation.eva.pipeline.jobs.JobTestUtils.*;
 import static org.junit.Assert.*;
 
 /**
  * Created by jmmut on 2015-10-14.
+ *
+ * The idea in this tests is to ensure that a variantJob with transform, load, stats and annotation; performs
+ * transform and load sequentially, and from then, stats and annot independently (concurrently, without time
+ * dependecies). Still we should decide what to do in cases like:
+ * - annot failed but stats could finish. Will the abrupt termination of annot, stop as well the stats?
  *
  * @author Jose Miguel Mut Lopez &lt;jmmut@ebi.ac.uk&gt;
  */
@@ -82,6 +67,9 @@ public class splitJobTest {
     private final Ticket end2 = new Ticket("finishing step2: ");
     private final Ticket start3 = new Ticket("starting step3:  ");
     private final Ticket end3 = new Ticket("finishing step3: ");
+    private final Ticket start4 = new Ticket("starting step4:  ");
+    private final Ticket end4 = new Ticket("finishing step4: ");
+    private final int duration = 3000;
 
 
     @Autowired
@@ -121,35 +109,141 @@ public class splitJobTest {
     }
     List<Ticket> order = SynchronizedList.decorate(new ArrayList<Ticket>());
 
-    /**
-     * The idea in this test is to ensure that a variantJob with transform, load, stats and annotation; performs
-     * transform and load sequentially, and from then, stats and annot independently (concurrently, without time
-     * dependecies). Still we should decide what to do in cases like:
-     * - annot failed but stats could finish. Will the abrupt termination of annot, stop as well the stats?
-     */
+    @Before
+    public void setUp() throws Exception {
+        order.clear();
+    }
+
     @Test
-    public void testSplitJob() throws Exception {
-        JobBuilder jobBuilder = jobBuilderFactory.get("splitJob");
-        JobExecution execution = jobRepository.createJobExecution("flow", new JobParameters());
-        Flow flow = new FlowBuilder<Flow>("splitflow")
+    public void testSplitJobOneLine() throws Exception {
+        String method = "testSplitJobOneLine";
+        JobBuilder jobBuilder = jobBuilderFactory.get(method);
+        JobExecution execution = jobRepository.createJobExecution(method + "Execution", new JobParameters());
+
+        Flow f2 = new FlowBuilder<Flow>(method + "subflow1").from(step2()).end();
+        Flow f3 = new FlowBuilder<Flow>(method + "subflow2").from(step3()).end();
+        jobBuilder.flow(step1())
+                .split(new SimpleAsyncTaskExecutor())
+                .add(f2, f3)
+                .next(step4())
+                .build().build().execute(execution);
+
+        check(method);
+    }
+
+    @Test
+    public void testLateSplitWorking() throws Exception {
+        String method = "testLateSplit";
+        JobBuilder jobBuilder = jobBuilderFactory.get(method);
+        JobExecution execution = jobRepository.createJobExecution(method + "Execution", new JobParameters());
+
+        Flow subflow3 = new FlowBuilder<Flow>(method + "subflow3").start(step3()).build();
+
+        Flow subflow2 = new FlowBuilder<Flow>(method + "subflow2")
+                .start(step2())
+                .split(new SimpleAsyncTaskExecutor())
+                .add(subflow3).build();
+
+        jobBuilder
+                .flow(step1())
+//                .split(new ConcurrentTaskExecutor())
+                .next(subflow2)
+                .build().build().execute(execution);
+
+        check(method);
+    }
+
+    @Test
+    public void testLateSplitFlow() throws Exception {
+        String method = "testLateSplit";
+        JobBuilder jobBuilder = jobBuilderFactory.get(method);
+        JobExecution execution = jobRepository.createJobExecution(method + "Execution", new JobParameters());
+
+        jobBuilder
+                .flow(step1())
+                .next(step2()).next(step4())
+//                .split(new ConcurrentTaskExecutor())
+                .split(new SimpleAsyncTaskExecutor())
+                .add(new FlowBuilder<Flow>(method + "subflow2").from(step3()).end())
+                .build().build().execute(execution);
+
+        check(method);
+    }
+    @Test
+    public void testSplitJobFlowsOneLine() throws Exception {
+        String method = "testSplitJobFlowsOneLine";
+        JobBuilder jobBuilder = jobBuilderFactory.get(method);
+        JobExecution execution = jobRepository.createJobExecution(method + "Execution", new JobParameters());
+
+        jobBuilder
+                .start(
+                        new FlowBuilder<Flow>(method + "flow")
+                                .start(step1())
+                                .split(new SimpleAsyncTaskExecutor())
+                                .add(
+                                        new FlowBuilder<Flow>(method + "subflow1").from(step2()).end(),
+                                        new FlowBuilder<Flow>(method + "subflow2").from(step3()).end())
+                                .build())
+                .build().build().execute(execution);
+
+        check(method);
+    }
+
+    @Test
+    public void testSplitJobFlowsOneLineWorking() throws Exception {
+        String method = "testSplitJobFlowsOneLineWorking";
+        JobBuilder jobBuilder = jobBuilderFactory.get(method);
+        JobExecution execution = jobRepository.createJobExecution(method + "Execution", new JobParameters());
+
+        jobBuilder
+                .start(
+                        new FlowBuilder<Flow>(method + "flow")
+                                .start(step1())
+                                .next(new FlowBuilder<Flow>(method + "branchflow")
+                                        .split(new SimpleAsyncTaskExecutor())
+                                        .add(
+                                                new FlowBuilder<Flow>(method + "subflow1").from(step2()).end(),
+                                                new FlowBuilder<Flow>(method + "subflow2").from(step3()).end())
+                                        .build())
+                                .build())
+                .build().build().execute(execution);
+
+        check(method);
+    }
+
+    @Test
+    public void testSplitJobWithFlowsWorking() throws Exception {
+        String method = "testSplitJobWithFlowsWorking";
+        JobBuilder jobBuilder = jobBuilderFactory.get(method);
+        JobExecution execution = jobRepository.createJobExecution(method + "Execution", new JobParameters());
+
+        Flow flow = new FlowBuilder<Flow>(method + "splitflow")
                 .split(new SimpleAsyncTaskExecutor())
 //                .split(new ConcurrentTaskExecutor())
 
-                .add(new FlowBuilder<Flow>("subflow1").from(step2()).end(),
-                        new FlowBuilder<Flow>("subflow2").from(step3()).end())
+                .add(
+                        new FlowBuilder<Flow>(method + "subflow1").from(step2()).end(),
+                        new FlowBuilder<Flow>(method + "subflow2").from(step3()).end())
                 .build();
-        Flow branchFlow = new FlowBuilder<Flow>("branchFlow").start(step1()).next(flow).build();
+
+        Flow branchFlow = new FlowBuilder<Flow>(method + "branchFlow").start(step1()).next(flow).build();
 
         jobBuilder.start(branchFlow).build().build().execute(execution);
+
+        check(method);
+    }
+
+    private void check(String method) {
+        logger.info("checking method " + method);
 
         for (Ticket t : order) {
             logger.info(t.message + t.time);
         }
+
         assertTrue("order.indexOf(end1) < order.indexOf(start2)", order.indexOf(end1) < order.indexOf(start2));
         assertTrue("order.indexOf(end1) < order.indexOf(start3)", order.indexOf(end1) < order.indexOf(start3));
         assertTrue("order.indexOf(start2) < order.indexOf(end3)", order.indexOf(start2) < order.indexOf(end3));
         assertTrue("order.indexOf(start3) < order.indexOf(end2)", order.indexOf(start3) < order.indexOf(end2));
-
     }
 
     public Step step1() {
@@ -158,7 +252,7 @@ public class splitJobTest {
             @Override
             public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                 order.add(new Ticket(start1.message, System.currentTimeMillis()));
-                Thread.sleep(5000);
+                Thread.sleep(duration);
                 order.add(new Ticket(end1.message, System.currentTimeMillis()));
                 return RepeatStatus.FINISHED;
             }
@@ -172,7 +266,7 @@ public class splitJobTest {
             @Override
             public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                 order.add(new Ticket(start2.message, System.currentTimeMillis()));
-                Thread.sleep(5000);
+                Thread.sleep(duration);
                 order.add(new Ticket(end2.message, System.currentTimeMillis()));
                 return RepeatStatus.FINISHED;
             }
@@ -186,7 +280,7 @@ public class splitJobTest {
             @Override
             public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                 order.add(new Ticket(start3.message, System.currentTimeMillis()));
-                Thread.sleep(5000);
+                Thread.sleep(duration);
                 order.add(new Ticket(end3.message, System.currentTimeMillis()));
                 return RepeatStatus.FINISHED;
             }
@@ -194,4 +288,17 @@ public class splitJobTest {
         return tasklet.build();
     }
 
+    public Step step4() {
+        StepBuilder step1 = stepBuilderFactory.get("step4");
+        TaskletStepBuilder tasklet = step1.tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                order.add(new Ticket(start4.message, System.currentTimeMillis()));
+                Thread.sleep(duration);
+                order.add(new Ticket(end4.message, System.currentTimeMillis()));
+                return RepeatStatus.FINISHED;
+            }
+        });
+        return tasklet.build();
+    }
 }
