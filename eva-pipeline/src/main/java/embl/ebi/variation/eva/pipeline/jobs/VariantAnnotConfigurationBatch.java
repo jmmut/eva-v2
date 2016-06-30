544ace6cbfc2bad293138f063eebb938ccd7ff8f
Diego Poggioli <diego@ebi.ac.uk>
2016-06-30 16:14:38 +0100

    Implementation of the variant annotation load steps using reader processor and writer - EVA-335

--- eva-pipeline/src/main/java/embl/ebi/variation/eva/pipeline/jobs/VariantAnnotConfigurationBatch.java
+++ eva-pipeline/src/main/java/embl/ebi/variation/eva/pipeline/jobs/VariantAnnotConfigurationBatch.java
@@ -13,70 +13,73 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package embl.ebi.variation.eva.pipeline.jobs;
 
-import embl.ebi.variation.eva.pipeline.steps.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import embl.ebi.variation.eva.pipeline.steps.VariantAnnotLoadBatch;
+import embl.ebi.variation.eva.pipeline.steps.VariantsAnnotCreate;
+import embl.ebi.variation.eva.pipeline.steps.VariantsAnnotGenerateInputBatch;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.builder.TaskletStepBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.env.Environment;
+
+/**
+ * @author Diego Poggioli
+ *
+ * Batch class to wire together:
+ * 1) variantsAnnotGenerateInputBatchStep - Dump a list of variants without annotations to be used as input for VEP
+ * 2) annotationCreate - run VEP
+ * 3) variantAnnotLoadBatchStep - Load VEP annotations into mongo
+ *
+ * At the moment is no longer possible to skip a single step like skipAnnotGenerateInput=true in the property
+ * To solve this we can implement the dynamic-workflow (https://github.com/EBIvariation/examples/tree/master/spring-batch-dynamic-workflow)
+ * or we can create a new Job class for each possible scenario
+ *
+ * TODO:
+ * - move variantsAnnotCreate and annotationCreate into a separate class to reuse across different jobs
+ */
 
 @Configuration
 @EnableBatchProcessing
-@Import(VariantJobArgsConfig.class)
-public class VariantAnnotConfiguration {
+@Import({VariantsAnnotGenerateInputBatch.class, VariantAnnotLoadBatch.class, VariantJobArgsConfig.class})
+public class VariantAnnotConfigurationBatch {
+    public static final String jobName = "variantAnnotBatchJob";
 
-    private static final Logger logger = LoggerFactory.getLogger(VariantAnnotConfiguration.class);
-    public static final String jobName = "variantAnnotJob";
+    @Autowired private JobBuilderFactory jobBuilderFactory;
 
-    @Autowired
-    private JobBuilderFactory jobBuilderFactory;
-    @Autowired
-    private StepBuilderFactory stepBuilderFactory;
-    @Autowired
-    JobLauncher jobLauncher;
-    @Autowired
-    Environment environment;
+    @Autowired private StepBuilderFactory stepBuilderFactory;
+
+    @Qualifier("variantsAnnotGenerateInputBatchStep")
+    @Autowired public Step variantsAnnotGenerateInputBatchStep;
+
+    @Qualifier("variantAnnotLoadBatchStep")
+    @Autowired private Step variantAnnotLoadBatchStep;
+
+    @Qualifier("annotationCreate")
+    @Autowired private Step annotationCreate;
 
     @Bean
-    public Job variantAnnotJob() {
+    public Job variantAnnotBatchJob() {
         JobBuilder jobBuilder = jobBuilderFactory
                 .get(jobName)
                 .incrementer(new RunIdIncrementer());
 
         return jobBuilder
-                .start(annotationGenerateInput())
-                .next(annotationCreate())
-                .next(annotationLoad())
+                .start(variantsAnnotGenerateInputBatchStep)
+                .next(annotationCreate)
+                .next(variantAnnotLoadBatchStep)
                 .build();
-    }
-
-    @Bean
-    public VariantsAnnotGenerateInput variantsAnnotGenerateInput(){
-        return new VariantsAnnotGenerateInput();
-    }
-
-    public Step annotationGenerateInput() {
-        StepBuilder step1 = stepBuilderFactory.get("annotationGenerateInput");
-        TaskletStepBuilder tasklet = step1.tasklet(variantsAnnotGenerateInput());
-
-        // true: every job execution will do this step, even if this step is already COMPLETED
-        // false: if the job was aborted and is relaunched, this step will NOT be done again
-        tasklet.allowStartIfComplete(false);
-        return tasklet.build();
     }
 
     @Bean
@@ -85,6 +88,7 @@ public class VariantAnnotConfiguration {
     }
 
     @Bean
+    @Qualifier("annotationCreate")
     public Step annotationCreate() {
         StepBuilder step1 = stepBuilderFactory.get("annotationCreate");
         TaskletStepBuilder tasklet = step1.tasklet(variantsAnnotCreate());
@@ -94,20 +98,4 @@ public class VariantAnnotConfiguration {
         tasklet.allowStartIfComplete(false);
         return tasklet.build();
     }
-
-    @Bean
-    public VariantsAnnotLoad variantsAnnotLoad(){
-        return new VariantsAnnotLoad();
-    }
-
-    public Step annotationLoad() {
-        StepBuilder step1 = stepBuilderFactory.get("annotationLoad");
-        TaskletStepBuilder tasklet = step1.tasklet(variantsAnnotLoad());
-
-        // true: every job execution will do this step, even if this step is already COMPLETED
-        // false: if the job was aborted and is relaunched, this step will NOT be done again
-        tasklet.allowStartIfComplete(false);
-        return tasklet.build();
-    }
-
 }
